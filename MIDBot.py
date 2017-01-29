61d3bf1030e77ac937f3de3e12ee403f6f41d82f
MarkFranciscus <mark.franciscus@mail.utoronto.ca>
2017-01-28 21:47:10 -0500

    db url

--- MIDBot.py
+++ MIDBot.py
@@ -15,7 +15,7 @@ mid_bot = Bot(command_prefix="!")
 # proc = subprocess.Popen('heroku config:get postgres://vppudcomfsevnd:bfbfe939ccd4505078be77bbc439bfce1b38d9e925ecd9d47fa18884c740b3e9@ec2-54-163-246-165.compute-1.amazonaws.com:5432/d4nsuj4jjqjaui -a fantasylcs', stdout=subprocess.PIPE, shell=True)
 # db_url = proc.stdout.read().decode('utf-8').strip()
 
-onn = psycopg2.connect("dbname='d4nsuj4jjqjaui' user='vppudcomfsevnd' host='ec2-54-163-246-165.compute-1.amazonaws.com' password='bfbfe939ccd4505078be77bbc439bfce1b38d9e925ecd9d47fa18884c740b3e9'")
+conn = psycopg2.connect("dbname='d4nsuj4jjqjaui' user='vppudcomfsevnd' host='ec2-54-163-246-165.compute-1.amazonaws.com' password='bfbfe939ccd4505078be77bbc439bfce1b38d9e925ecd9d47fa18884c740b3e9'")
 
 # conn = psycopg2.connect('postgres://vppudcomfsevnd:bfbfe939ccd4505078be77bbc439bfce1b38d9e925ecd9d47fa18884c740b3e9@ec2-54-163-246-165.compute-1.amazonaws.com:5432/d4nsuj4jjqjaui')
 # conn = psycopg2.connect(
