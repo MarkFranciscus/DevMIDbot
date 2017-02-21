628d8c3e06667b9cd7c16ebc7419938544348666
MarkFranciscus <mark.franciscus@mail.utoronto.ca>
2017-02-21 15:07:04 -0500

    shitter
    roman numerals

--- LeagueStats.py
+++ LeagueStats.py
@@ -5,9 +5,9 @@ riotapi.set_region("NA")
 riotapi.set_api_key("e0675e35-f858-48d1-ad1b-cfac6fb6955c")
 
 tierToNumber = {'bronze': 0, 'silver': 1, 'gold': 2, 'platinum': 3, 'diamond': 4, 'master': 5, 'challenger': 6}
-numberToTier = {0: 'bronze', 1: 'silver', 2: 'gold', 3: 'platinum', 4: 'diamond', 5: 'master', 6: 'challenger'}
+numberToTier = {0: 'bronze', 1: 'Silver', 2: 'Gold', 3: 'Platinum', 4: 'Diamond', 5: 'Master', 6: 'Challenger'}
 divisionToNumber = {'five': 0, 'four': 1, 'three': 2, 'two': 3, 'one':4}
-numberToDivision = {0:'five', 1:'four',2:'three',3:'two',4:'one'}
+numberToDivision = {0:'V', 1:'IV',2:'III',3:'II',4:'I'}
 
 def shitter(usernames):
     # summoner1 = riotapi.get_summoner_by_name("rythemkiller")
@@ -27,12 +27,6 @@ def shitter(usernames):
             eloList.append([tierToNumber[str(currentUser[1].tier)[5:]], divisionToNumber[str(currentUser[1].entries[0].division)[9:]], currentUser[1].entries[0].league_points, currentUser[1].entries[0].summoner])
             eloList = (sorted(eloList, key=itemgetter(0, 1, 2)))
             eloList.pop()
-
-    # mark = riotapi.get_league_entries_by_summoner(summoners=summoner1)
-    # ivan = riotapi.get_league_entries_by_summoner(summoners=summoner2)
-    # daniel = riotapi.get_league_entries_by_summoner(summoners=summoner3)
-
-    # eloList = [[tierToNumber[str(mark[1].tier)[5:]], divisionToNumber[str(mark[1].entries[0].division)[9:]], mark[1].entries[0].league_points, mark[1].entries[0].summoner], [tierToNumber[str(ivan[1].tier)[5:]], divisionToNumber[str(ivan[1].entries[0].division)[9:]], ivan[1].entries[0].league_points, ivan[1].entries[0].summoner], [tierToNumber[str(daniel[1].tier)[5:]], divisionToNumber[str(daniel[1].entries[0].division)[9:]], daniel[1].entries[0].league_points, daniel[1].entries[0].summoner]]
 
 
     return str(eloList[0][3].name) + " is the shitter - " + str( numberToTier[eloList[0][0]]) + " " + str(numberToDivision[eloList[0][1]]) + " " + str(eloList[0][2]) + " LP"
@@ -54,13 +48,6 @@ def last10Games(username):
                if participant.summoner == summoner:
                    summonerFound = True
                    break
-                   # if match.blue_team.win:
-                   #     win +=1
-                   #     print("win " + str(win))
-                   #
-                   #     break;
-                   # else:
-                   #      loss +=1
             if not summonerFound and match.blue_team.win:
                 loss += 1
             elif summonerFound and not match.blue_team.win:
@@ -68,9 +55,9 @@ def last10Games(username):
             else:
                 win += 1
             print("win " + str(win) + ", lose " + str(loss))
-            # if not summonerFound and match.red_team.win:
-            #     win +=1
-        # if match.queue == 'flex':
+
 # def elo(username);
 #
 # last10Games("rythemkiller")
+
+# print(shitter([('Rythemkiller                                                                                        ',), ('CourageousFalcon                                                                                    ',), ('Flailure                                                                                            ',)]))
