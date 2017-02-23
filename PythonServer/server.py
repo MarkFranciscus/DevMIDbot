701a288a5df419238273e655000985e3059b571f
MarkFranciscus <mark.franciscus@mail.utoronto.ca>
2017-02-23 13:29:21 -0500

    daemon

--- PythonServer/server.py
+++ PythonServer/server.py
@@ -2,16 +2,5 @@ import time
 from daemon import runner
 import MIDBot
 
-class App():
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/tmp/foo.pid'
-        self.pidfile_timeout = 5
-    def run(self):
-        MIDBot.MIDBot()
-
-app = App()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+with daemon.DaemonContext():
+    MIDBot.MIDBot()
