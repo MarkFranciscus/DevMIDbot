60cf8ae5e74a8449b02c34d9375b0bc597b69a5e
MarkFranciscus <mark.franciscus@mail.utoronto.ca>
2017-05-12 00:18:51 -0400

    testing webhooks

--- PythonServer/MIDBot.py
+++ PythonServer/MIDBot.py
@@ -216,9 +216,9 @@ def commands():
 
     yield from mid_bot.say(commands)
 
-# @mid_bot.command()
-# @asyncio.coroutine
-# def lcs():
-
+@mid_bot.command()
+@asyncio.coroutine
+def lcs():
+    yield from mid_bot.say("webhooks")
 
 mid_bot.run(BotInfo.BOT_TOKEN)
