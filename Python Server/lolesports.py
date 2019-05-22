a39dbf36be8fef2612e73842cbcb8b696d50fb5a
MarkF <mark.franciscus@hotmail.com>
2019-05-22 16:21:53 -0400

    fixed bug in standings

--- Python Server/lolesports.py
+++ Python Server/lolesports.py
@@ -15,11 +15,7 @@ def get_team_ids():
     return ids
 
 def get_standings():
-    # Find current split and
-    for split in rawData["highlanderTournaments"]:
-        if split["title"] == "lcs_2019_spring":
-            brackets = split["brackets"]
-            rosters = split["rosters"]
+    brackets, rosters = find_current_split()
 
     for bracket in brackets.values():
         if bracket["name"] == "regular_season":
@@ -31,35 +27,37 @@ def get_standings():
         standings[i+1] = [rosters[x["roster"]]["name"] for x in raw_standings[i]]
     
     teams = rawData["teams"]
-    result = []
-    num_teams = 0
-    for i in range(1, 11, 1):
+    result = {key:[] for key in range(1, 11, 1)}
+    num_teams = 1
+    for i in range(1, 10, 1):
         placement = []
         for x in standings[i]:
-            num_teams += 1
             for team in teams:
                 if x == team["acronym"]:
                     placement.append(str(team["name"]))
-        standings[num_teams] = placement
-    print(standings)
+        result[num_teams] = placement
+        num_teams += len(standings[i])
+
+def find_current_split():
+    """Find current split and roster"""
+    for split in rawData["highlanderTournaments"]:
+        if split["title"] == "lcs_2019_spring":
+            brackets = split["brackets"]
+            rosters = split["rosters"]
+    return brackets, rosters
 
 def get_slug(ids):
     teams = rawData["teams"]
-    # print(teams)
     slugs = []
     for x in ids:
         for y in teams:
-            # print (x, y["id"])
             if x == y["id"]:
-                # print (x, y["id"])
                 slugs.append(y["slug"])
                 break
     print(slugs)
-# parse the text of the URL
-# q = get_team_ids()
-# print(q)
+
 print(get_standings())
-# get_slug(q)
+
 # Each row is: Rank/Team/Wins/Losses
 format_string = "%-4s %-20s %-s %-s"
 
