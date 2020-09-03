1eee08b2b494703d424309ef4c08b9f39966f542
Mark Franciscus <mark.franciscus@hotmail.com>
2020-09-03 12:48:54 -0400

    change dtypes of teams from int to bool

--- Python Server/utility.py
+++ Python Server/utility.py
@@ -805,6 +805,8 @@ async def parse_gamedata(gameID, leagueID, engine):
 
     teams.rename(columns=team_rename_columns, inplace=True)
     teams.drop('kill_event', axis=1, inplace=True)
+    teams = teams.astype({'win': 'bool', 'first_blood': 'bool', 'under_30': ' bool'})
+
 
     participants.drop_duplicates(
         ['gameid', 'summoner_name', 'timestamp'], inplace=True)
